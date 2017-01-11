b0187f694710a580ba3091ffcf64fbcd99f73232
gzhk <grzegorz.hejduk@gft.com>
2017-01-11 10:25:49 +0100

    Clean application

---
+++ src/main/java/com/gft/ChallengeOneApplication.java
@@ -0,0 +1,12 @@
+package com.gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeOneApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gft/ChallengeOneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChallengeOneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
