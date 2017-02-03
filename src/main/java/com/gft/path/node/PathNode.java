87813d789c8e1a21447c9183429fae9f28c93e4e
gzhk <grzegorz.hejduk@gft.com>
2017-02-03 14:20:32 +0100

    Step02 - right direction

--- src/main/java/com/gft/path/node/PathNode.java
+++ src/main/java/com/gft/path/node/PathNode.java
@@ -1,10 +1,15 @@
 package com.gft.path.node;
 
+import com.gft.node.CannotRetrieveChildren;
 import com.gft.node.Node;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 public final class PathNode implements Node<Path> {
 
@@ -15,12 +20,30 @@ public final class PathNode implements Node<Path> {
     }
 
     @Override
-    public Collection<Node<Path>> children() {
-        return null;
+    public Collection<Node<Path>> children() throws CannotRetrieveChildren {
+        ArrayList<Node<Path>> nodes = new ArrayList<>();
+
+        try {
+            Files.list(path).map(PathNode::new).forEach(nodes::add);
+        } catch (IOException e) {
+            throw new CannotRetrieveChildren("Cannot list files in path: " + path  , e);
+        }
+
+        return nodes;
     }
 
     @Override
     public Path getPayload() {
         return path;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        return o instanceof PathNode && Objects.equals(path, ((PathNode) o).path);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path);
     }
 }
